50f3e251599b2f97e3fa276d6b96f754617a527b
20jun <ex_xe@naver.com>
2021-07-26 12:16:19 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const app = express();
-const port = 8000
+const port = 8000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://yj:yj@boilerplate.r9lww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
-}).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err))
+const config = require("./config/key");
 
+const { User } = require("./models/USer");
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//application/json
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send("Hello World"));
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World dev"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
